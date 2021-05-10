87d8cabe02b8a82c493e10c277f7ec9ce15c0038
Jose Pablo Ponce <pon19092@uvg.edu.gt>
2021-05-10 12:39:47 -0600

    Instalación de dependencias

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+	semi: ['error', 'never'],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'no-undef': 'off',
+  },
+};
